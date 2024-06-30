4d4869a5ddce9cf1d2ffd290443fe32ff2b8666d
Luis Filipe e Silva <137459500+luisfilipex@users.noreply.github.com>
2024-06-29 22:53:05 -0300

    Delete controller directory

--- controller/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//aqui fiz a config do db
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "controle_financeiro";
-?>
-
--- controller/contas_pagar.php
+++
@@ -1,8 +0,0 @@
-
-// cria a tabale de conta a pagar
-CREATE TABLE contas_pagar (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente VARCHAR(255) NOT NULL,
-    data_pagamento DATE NOT NULL,
-    valor DECIMAL(10,2) NOT NULL
-);
